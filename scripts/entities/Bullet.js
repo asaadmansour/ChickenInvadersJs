191cde8cb31aef780e1613d21608079eccfba9ac
Abdulrahman <movieshome160@gmail.com>
2026-01-17 06:55:47 +0200

    feat: add Player and Bullet entity classes

---
+++ scripts/entities/Bullet.js
@@ -0,0 +1,41 @@
+export class Bullet {
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+        
+        this.width = 8;
+        this.height = 16;
+
+        this.moveSpeed = 10;
+        
+        this.isActive = true;
+    }
+
+    /**
+     * Move bullet upward
+     */
+    move() {
+        this.y -= this.this.moveSpeed;
+    }
+
+  
+    /**
+     * Get bounding box for collision
+     * @returns {Object} {x, y, width, height}
+     */
+    getBounds() {
+        return {
+            x: this.x,
+            y: this.y,
+            width: this.width,
+            height: this.height
+        };
+    }
+
+    /**
+     * Mark bullet for removal
+     */
+    deactivate() {
+        this.isActive = false;
+    }
+}
