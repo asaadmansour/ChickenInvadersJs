deb39e39b893723c6758cbe57c58e20e6e2cda8a
asaadmansour <as89aadmansour@gmail.com>
2026-01-17 13:53:33 +0200

    WIP: local changes

--- scripts/entities/Chicken.js
+++ scripts/entities/Chicken.js
@@ -33,6 +33,7 @@ export class Chicken {
   }
   draw(ctx) {
     ctx.drawImage(this.image, this.x, this.y, this.width, this.height);
+    ctx.drawImage(this.image, this.x, this.y, this.width, this.height);
   }
 
   getBounds() {
