1e4a0803929df90e2aa81b689fc1aec55969e920
Mohamed Sameh <mohamed.kholy2011@gmail.com>
2026-01-23 11:44:03 +0200

    Added Intro sound

--- scripts/pages/main.js
+++ scripts/pages/main.js
@@ -1,8 +1,17 @@
 const playBtn = document.querySelector(".play");
-playBtn.addEventListener("click",()=> {
-    window.location.href = "pages/game.html"
-})
+playBtn.addEventListener("click", () => {
+  window.location.href = "pages/game.html";
+});
 const instructionsBtn = document.querySelector(".instructions");
-instructionsBtn.addEventListener("click",()=> {
-    // window.location.href = "pages/game.html"
-})
+instructionsBtn.addEventListener("click", () => {
+  // window.location.href = "pages/game.html"
+});
+
+// Intro sound
+window.addEventListener("DOMContentLoaded", function () {
+  const introAudio = new Audio("../assets/audio/Intro.mp3");
+  introAudio.preload = "auto";
+  introAudio.volume = 0.7;
+  introAudio.loop = true;
+  introAudio.play();
+});
