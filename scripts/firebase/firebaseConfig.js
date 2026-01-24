8957eb09238ce7e1878861d3d6e2ab54a5296e50
Mohamed Sameh <mohamed.kholy2011@gmail.com>
2026-01-25 01:09:08 +0200

    Revert "Added .env"
    This reverts commit 679f7b880993cbcf6628962cb809e57e79f4f4db.

--- scripts/firebase/firebaseConfig.js
+++ scripts/firebase/firebaseConfig.js
@@ -1,14 +1,16 @@
+// scripts/firebase/firebaseConfig.js
+
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.8.0/firebase-app.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/12.8.0/firebase-firestore.js";
 
-// Firebase configuration from .env
+// Firebase configuration
 const firebaseConfig = {
-  apiKey: import.meta.env.FIREBASE_API_KEY,
-  authDomain: import.meta.env.FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.FIREBASE_APP_ID,
+  apiKey: "AIzaSyDJno6qzd412tft1jNv9v-Odhut-YDDeh0",
+  authDomain: "chicken-invaders-scoreboard.firebaseapp.com",
+  projectId: "chicken-invaders-scoreboard",
+  storageBucket: "chicken-invaders-scoreboard.firebasestorage.app",
+  messagingSenderId: "569264713165",
+  appId: "1:569264713165:web:40c4c7b0b0fdf381dae884",
 };
 
 // Initialize Firebase App
